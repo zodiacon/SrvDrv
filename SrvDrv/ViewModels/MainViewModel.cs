f5e5971afedf8ef15becfb2512184a9dcd41b320
Pavel Yosifovich <zodiacon@live.com>
2016-07-30 18:53:51 -0400

    drivers, services and search

--- SrvDrv/ViewModels/MainViewModel.cs
+++ SrvDrv/ViewModels/MainViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -19,7 +20,8 @@ namespace SrvDrv.ViewModels {
         public IEnumerable<ServiceViewModel> Services {
             get {
                 if(_services == null) {
-                    _services = ServiceController.GetServices().Concat(ServiceController.GetDevices()).Select(svc => new ServiceViewModel(svc));
+                    _services = ServiceController.GetServices().Concat(ServiceController.GetDevices()).OrderBy(svc => svc.ServiceName)
+                        .Select(svc => new ServiceViewModel(svc));
                 }
                 return _services;
             }
@@ -46,5 +48,53 @@ namespace SrvDrv.ViewModels {
             }
         }
 
+        private bool _showServices = true;
+
+        public bool ShowServices {
+            get { return _showServices; }
+            set {
+                if(SetProperty(ref _showServices, value)) {
+                    UpdateFilter();
+                }
+            }
+        }
+
+        private bool _showDrivers = true;
+
+        public bool ShowDrivers {
+            get { return _showDrivers; }
+            set {
+                if(SetProperty(ref _showDrivers, value)) {
+                    UpdateFilter();
+                }
+            }
+        }
+
+        private string _searchText = string.Empty;
+
+        public string SearchText {
+            get { return _searchText; }
+            set {
+                if(SetProperty(ref _searchText, value)) {
+                    UpdateFilter();
+                }
+            }
+        }
+
+        void UpdateFilter() {
+            var view = CollectionViewSource.GetDefaultView(_services);
+            view.Filter = obj => {
+                var svc = (ServiceViewModel)obj;
+                int type = (int)svc.Service.ServiceType;
+                if(!ShowServices && type >= 0x10)
+                    return false;
+
+                if(!ShowDrivers && type < 0x10)
+                    return false;
+
+                var text = SearchText.ToLower();
+                return svc.Name.ToLower().Contains(text) || svc.DisplayName.ToLower().Contains(text);
+            };
+        }
     }
 }
--- SrvDrv/ViewModels/ServiceViewModel.cs
+++ SrvDrv/ViewModels/ServiceViewModel.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Microsoft.Win32;
+using Microsoft.Win32.SafeHandles;
 using Prism.Commands;
 using Prism.Mvvm;
 using Zodiacon.WPF;
@@ -21,5 +24,85 @@ namespace SrvDrv.ViewModels {
 
         public string Name => Service.ServiceName;
         public string DisplayName => Service.DisplayName;
+
+        public string Type {
+            get {
+                switch(Service.ServiceType & (~ServiceType.InteractiveProcess)) {
+                    case ServiceType.FileSystemDriver:
+                        return "File System Driver";
+
+                    case ServiceType.Win32OwnProcess:
+                        return "Service (Own Process)";
+
+                    case ServiceType.Win32ShareProcess:
+                        return "Service (Share Process)";
+
+                    case ServiceType.KernelDriver:
+                        return "Kernel Driver";
+
+                    case ServiceType.RecognizerDriver:
+                        return "Recognizer Driver";
+                }
+                return Service.ServiceType.ToString();
+            }
+        }
+
+        public string Status => Service.Status.ToString();
+
+        string _description;
+        public string Description => _description ?? (_description = GetDescription());
+
+        [DllImport("advapi32", CharSet = CharSet.Unicode)]
+        private static extern int RegLoadMUIString(SafeRegistryHandle hKey, string value, StringBuilder output, int count, out int size, uint flags, string directory);
+
+        static StringBuilder _descString = new StringBuilder(1024);
+
+        private string GetDescription() {
+            using(var key = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Services\" + Name)) {
+                int size = _descString.Capacity;
+                int error = RegLoadMUIString(key.Handle, "Description", _descString, size, out size, 0, null);
+                var desc = string.Empty;
+                if(error == 0)
+                    desc = _descString.ToString();
+                return desc;
+            }
+        }
+
+        public string StartType => Service.StartType.ToString();
+
+        public string DependsOn => string.Join(", ", Service.ServicesDependedOn.Select(svc => svc.ServiceName));
+
+        public string DependentServices => string.Join(", ", Service.DependentServices.Select(svc => svc.ServiceName));
+
+        public string Icon => GetIcon();
+
+        private string GetIcon() {
+            var type = Service.ServiceType & ~ServiceType.InteractiveProcess;
+
+            switch(type) {
+                case ServiceType.Win32OwnProcess:
+                case ServiceType.Win32ShareProcess:
+                    return Service.Status == ServiceControllerStatus.Running ? "/icons/gear_run.ico" : "/icons/gear_stop.ico";
+
+                case ServiceType.FileSystemDriver:
+                    return "/icons/data.ico";
+
+            }
+            return "/icons/driver.ico";
+        }
+
+        public string SupportedOperations {
+            get {
+                var ops = new List<string>(4);
+                if(Service.CanStop)
+                    ops.Add("Can Stop");
+                if(Service.CanShutdown)
+                    ops.Add("Can Shutdown");
+                if(Service.CanPauseAndContinue)
+                    ops.Add("Can Pause and Continue");
+                return string.Join(", ", ops);
+            }
+        }
+
     }
 }
